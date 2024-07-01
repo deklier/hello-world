031c7bc5b45ace7bed3245cfd57fbf33159e6b89
deklier <deklier@gmail.com>
2024-07-01 17:13:22 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+        <h1>Hello World!</h1>
+        <p1>Meu primeiro Hello World!</p1>
+    </div>
+  );
+}
+
+export default App;
